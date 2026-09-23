64f98fda72d5a8630c14f06483df589aca7fd01e
Markos Andres <markos@andres.com>
2026-09-23 11:20:02 -0600

    Initial commit

---
+++ src/main/java/com/marcos/incode_home_task/IncodeHomeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.incode_home_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncodeHomeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncodeHomeTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/incode_home_task/IncodeHomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.incode_home_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncodeHomeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
